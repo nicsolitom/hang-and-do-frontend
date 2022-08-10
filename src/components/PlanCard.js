af59ebfafdc72443f255bed548b2bb3a340ef0b4
Nic Solitom <nicsolitom@gmail.com>
2022-08-10 20:01:17 +0200

    Adding and correcting how plans are shown in list.

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
- 
-function PlanCard ( { title, description, _id } ) {
-    
+
+function PlanCard({ title, description, dateTime, _id }) {
   return (
-    <div className="PlanCard card">
-      <Link to={`/plans/${_id}`}>
+    <Link to={`/plans/${_id}`}>
+      <div className='PlanCard card'>
         <h3>{title}</h3>
-      </Link>
-      <p style={{ maxWidth: "400px" }}>{description} </p>
-    </div>
+        {dateTime && (
+          <p style={{ maxWidth: "400px" }}>
+            <strong>
+              &#62; {dateTime.slice(0, 10)} | {dateTime.slice(11, 16)}
+            </strong>
+          </p>
+        )}
+        <p style={{ maxWidth: "400px" }}>{description} </p>
+      </div>
+    </Link>
   );
 }
- 
+
 export default PlanCard;
